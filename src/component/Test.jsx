4784aab5649ece2c05b83fa8f27412ee76a318bb
abatman92 <lyamzin505@gmail.com>
2022-02-07 12:18:37 +0300

    commit

---
+++ src/component/Test.jsx
@@ -0,0 +1,100 @@
+import { ErrorMessage } from "@hookform/error-message";
+import React, { useEffect } from "react";
+import { useForm, useFieldArray} from "react-hook-form";
+
+export function Test() {
+    const {
+      register,
+      handleSubmit,
+      control,
+      formState: { errors },
+    } = useForm({criteriaMode: 'all', mode: 'onBlur'});
+    const {fields, append, remove} = useFieldArray({
+        control,
+        name: "person",
+    })
+    useEffect(() => {
+      append({ name: "", email: "" });
+    }, []);
+  return (
+    <>
+      <h1>Приглашение на вечеринку</h1>
+      <form
+        className="form"
+        onSubmit={handleSubmit((data) => {
+          alert(JSON.stringify(data));
+        })}
+      >
+        {fields.map((field, index) => {
+          return (
+            <div key={index} className="container">
+              <div className="inp-container">
+                <label className="form-label">Имя</label>
+                <input
+                  className="form-control"
+                  {...register(`person.${index}.name`, {
+                    required: "Это поле обязательно",
+                  })}
+                />
+                <ErrorMessage errors={errors} name={`person.${index}.name`} render={({ message }) => <p>{message}</p>}/>
+              </div>
+              <div className="inp-container">
+                <label className="form-label">E-mail</label>
+                <input
+                  className="form-control"
+                  {...register(`person.${index}.email`, {
+                    required: "Это поле обязательно",
+                    pattern: {
+                      value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                      message: "Некорректный формат e-mail"
+                    }
+                  })}
+                />
+                <ErrorMessage errors={errors} name={`person.${index}.email`} render={({ message }) => <p>{message}</p>}/>
+              </div>
+              {index > 0 && (
+                <button type="button" onClick={() => remove(index)}>
+                  X
+                </button>
+              )}
+            </div>
+          );
+        })}
+        <button
+          className="button"
+          type="button"
+          onClick={() => append({ name: "", email: "" })}
+        >
+          Добавить гостя
+        </button>
+        <label>Название мероприятия</label>
+        <input
+          type="text"
+          {...register("title", {
+            required: "Это поле обязательно",
+          })}
+        />
+        <ErrorMessage errors={errors} name='title' render={({ message }) => <p>{message}</p>}/>
+        <label>Описание мероприятия</label>
+        <textarea
+          {...register("description", {
+            required: "Это поле обязательно",
+          })}
+        />
+        <ErrorMessage errors={errors} name="description" render={({ message }) => <p>{message}</p>}/>
+        <label>Дата и время</label>
+        <input
+          type="datetime-local"
+          {...register("timestamp", {
+            required: "Это поле обязательно",
+          })}
+        />
+        <ErrorMessage errors={errors} name="timestamp" render={({ message }) => <p>{message}</p>}/>
+        <input type="submit" className="button" />
+      </form>
+    </>
+  );
+}
+
+  
+  
